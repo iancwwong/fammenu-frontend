5d39968db1ef63c06c516c77447539ca7225383e
Ian Wong <wong.iancw@gmail.com>
2021-01-05 09:30:17 +1100

    Setting up create page

--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 
-const CreatePage = () => (
-    <div>
-        <Navbar />
-        <h2>Create Page!</h2>
-    </div>
-);
+export default class CreatePage extends React.Component {
 
-export default CreatePage;
+    handleCreateFoodItem = (eventObj) => {
+
+        eventObj.preventDefault();
+
+        // Sanitise inputs
+        let name = eventObj.target.elements.name.value.trim();
+        let cuisine = eventObj.target.elements.cuisine.value.trim();
+        let labels = eventObj.target.elements.labels.value
+                        .trim().split(";")
+                        .map((label) => label.trim())
+                        .filter((label) => label);
+
+        const newFoodItem = {
+            name: name,
+            cuisine: cuisine,
+            labels: labels
+        };
+
+        console.log("Creating new food item: ")
+        console.log(newFoodItem);
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar />
+
+                <h2>Create Page!</h2>
+                <form onSubmit={this.handleCreateFoodItem}>
+                    <label>
+                        Name:
+                        <input type="text" name="name" />
+                    </label>
+                    <br></br>
+                    <label>
+                        Cuisine:
+                        <input type="text" name="cuisine" />
+                    </label>
+                    <br></br>
+                    <label>
+                        Labels:
+                        <input type="text" name="labels" />
+                    </label>
+                    <br></br>
+                    <button>Create</button>
+                </form>
+            </div>
+        );
+    }
+}
