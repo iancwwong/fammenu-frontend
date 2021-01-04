fa4c40d1653c343bab86ecc66dfb3d072d135f4a
Ian Wong <wong.iancw@gmail.com>
2021-01-05 08:46:50 +1100

    Removing cuisine label for display

--- src/components/searchPage/FoodItem.js
+++ src/components/searchPage/FoodItem.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 const FoodItem = (props) => (
     <div>
-        <ul>
-            <li key={props.foodItem.id + props.foodItem.name}>{props.foodItem.name}</li>
-            <li key={props.foodItem.id + props.foodItem.cuisine}>{props.foodItem.cuisine}</li>
-        </ul>
+        <li key={props.foodItem.id + props.foodItem.name}>{props.foodItem.name}</li>
     </div>
 );
 
