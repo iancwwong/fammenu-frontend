757bb2abb3acec01839d773fd228e987424e9a19
Ian Wong <wong.iancw@gmail.com>
2021-01-05 22:55:01 +1100

    Making edit page a component

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
-const EditPage = (props) => (
-    <div>
-        <p>This is the edit page!</p>
-        <p>{props.match.params.foodItemId}</p>
-    </div>
-);
+export default class EditPage extends React.Component {
 
-export default EditPage;
+    state = {
+        foodItemId: this.props.match.params.foodItemId
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <h2>Edit Food Item: {this.state.foodItemId}</h2>
+                <p>{JSON.stringify(this.props)}</p>
+                <p>{this.props.match.params.foodItemId}</p>
+            </div>
+        );
+    }
+}
