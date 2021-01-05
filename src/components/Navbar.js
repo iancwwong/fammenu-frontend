2b7eed67c77ef2f554639fe98f289b63cadf58a8
Ian Wong <wong.iancw@gmail.com>
2021-01-06 09:26:29 +1100

    Trying to make navbar nicer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 const Navbar = () => (
     <div>
         <nav>
-            <Link to="/">Home</Link> |
-            <Link to="/search">Search</Link> |
-            <Link to="/create">Create</Link>
+            <Link to="/"><HomeIcon /></Link> |
+            <Link to="/search"><SearchIcon /></Link> |
+            <Link to="/create"><AddCircleIcon /></Link>
         </nav>
     </div>
 );
