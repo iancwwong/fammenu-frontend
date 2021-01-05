9c9fd64430eae33445e157fdcbae2c89556f64c0
Ian Wong <wong.iancw@gmail.com>
2021-01-06 09:44:43 +1100

    Improving home page font

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Typography from '@material-ui/core/Typography';
 
 const HomePage = () => (
     <div>
         <Navbar />
-        <h2>Welcome to fammenu-frontend!</h2>
+
+        <Typography variant="h6">
+            Welcome to the fammenu-frontend!
+        </Typography>
     </div>
 );
 
