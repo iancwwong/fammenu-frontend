b81f305e2a78092cbf3e80bb65e8530e6eed64e3
Ian Wong <wong.iancw@gmail.com>
2021-01-07 23:38:46 +1100

    Error handling for update form

--- src/components/searchPage/EditFoodItemDialog.js
+++ src/components/searchPage/EditFoodItemDialog.js
@@ -5,41 +5,142 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const dataClient = require('../../utils/DataClient');
 
-const EditFoodItemDialog = (props) => (
-    <Dialog 
-        open={!!props.foodItemToEdit}
-        onClose={props.resetFoodItemToEdit}
-    >
-        <DialogTitle>Edit Food Item</DialogTitle>
+export default class EditFoodItemDialog extends React.Component {
 
-        <DialogContent>
-            <DialogContentText>
-                Editing '{props.foodItemToEdit.name}'...
-            </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-            <Button onClick={props.resetFoodItemToEdit} color="default">
-                Cancel
-            </Button>
-            <Button 
-                onClick={() => dataClient.updateFoodItemById(
-                    props.foodItemToEdit,
-                    
-                    // Success callback
-                    props.resetFoodItemToEditAfterSuccessfulEdit,
+    state = {
+        nameError: undefined,
+        cuisineError: undefined,
+        labelsError: undefined
+    }
 
-                    // Error callback
-                    (err) => console.error(err))} 
-                style={{color: "green"}}
+    handleValidateUpdateForm = () => {
+        // Get values of update fields
+        let name = document.getElementById('editName').value.trim();
+        let cuisine = document.getElementById('editCuisine').value.trim();
+        let labels = document.getElementById('editLabels').value
+                        .trim().split(";")
+                        .map((label) => label.trim())
+                        .filter((label) => label);          // Remove empty labels
+
+        // Validate fields
+        if (!name) {
+            this.setState(() => ({
+                nameError: "Name not given"
+            }));
+        } else {
+            this.setState(() => ({
+                nameError: undefined
+            }))
+        }
+
+        if (!cuisine) {
+            this.setState(() => ({
+                cuisineError: "Cuisine not given"
+            }));
+        } else {
+            this.setState(() => ({
+                cuisineError: undefined
+            }));
+        }
+    }
+
+    handleUpdateFoodItem = (originalFoodItem) => {
+        // Get values of update fields
+        let name = document.getElementById('editName').value.trim();
+        let cuisine = document.getElementById('editCuisine').value.trim();
+        let labels = document.getElementById('editLabels').value
+                        .trim().split(";")
+                        .map((label) => label.trim())
+                        .filter((label) => label);          // Remove empty labels
+
+        const foodItemToUpdate = {
+            id: originalFoodItem.id,
+            name: name,
+            cuisine: cuisine,
+            labels: labels
+        };
+        console.log("Updating food item: ");
+        console.log(foodItemToUpdate);
+                                    
+        dataClient.updateFoodItemById(
+            foodItemToUpdate,
+            this.props.resetFoodItemToEditAfterSuccessfulEdit,
+
+            // Error callback
+            (err) => console.error(err));
+    }
+
+    render() {
+        return (
+            <Dialog 
+                open={!!this.props.foodItemToEdit}
+                onClose={this.props.resetFoodItemToEdit}
             >
-                Update
-            </Button>
-        </DialogActions>
-
-    </Dialog>
-);
-
-export default EditFoodItemDialog;
+                <DialogTitle>Edit Food Item</DialogTitle>
+        
+                <DialogContent>
+                    <DialogContentText>
+                        Details for '{this.props.foodItemToEdit.name}'...
+                    </DialogContentText>
+        
+                    <TextField
+                        autoFocus
+                        id="editName"
+                        label="Name"
+                        required
+                        error={!!this.state.nameError}
+                        helperText={this.state.nameError}
+                        type="text"
+                        fullWidth
+                        defaultValue={this.props.foodItemToEdit.name}
+                        onChange={this.handleValidateUpdateForm}
+                    />
+        
+                    <TextField
+                        autoFocus
+                        id="editCuisine"
+                        label="Cuisine"
+                        required
+                        error={!!this.state.cuisineError}
+                        helperText={this.state.cuisineError}
+                        type="text"
+                        fullWidth
+                        defaultValue={this.props.foodItemToEdit.cuisine}
+                        onChange={this.handleValidateUpdateForm}
+                    />
+        
+                    <TextField
+                        autoFocus
+                        id="editLabels"
+                        label="Labels"
+                        type="text"
+                        fullWidth
+                        defaultValue={this.props.foodItemToEdit.labels.join("; ")}
+                        onChange={this.handleValidateUpdateForm}
+                    />
+        
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.props.resetFoodItemToEdit} color="default">
+                        Cancel
+                    </Button>
+                    <Button 
+                        disabled={
+                            this.state.nameError ||
+                            this.state.cuisineError ||
+                            this.state.labelsError}
+                        onClick={() => this.handleUpdateFoodItem(this.props.foodItemToEdit)}
+                        style={{color: "green"}}
+                    >
+                        Update
+                    </Button>
+                </DialogActions>
+        
+            </Dialog>
+        );
+    }
+}
