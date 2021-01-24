d3c30f9585f44f6fcbbd3085396b27c6540def14
Ian Wong <wong.iancw@gmail.com>
2021-01-24 19:24:40 +1100

    Removing fixed scrollable component for search result

--- src/components/viewFoodItemsPage/SearchResults.js
+++ src/components/viewFoodItemsPage/SearchResults.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import FoodItemResult from './FoodItemResult';
-import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
@@ -20,21 +19,19 @@ const SearchResults = (props) => (
 
         {
             (props.foundFoodItems && props.foundFoodItems.length > 0) &&
-            <Paper style={{maxHeight: 400, overflow: 'auto'}}>
-                <List>
-                    {props.foundFoodItems.map((foodItem) => 
-                    (
-                        <ListItem button onClick={() => props.triggerFoodItemUpdate(foodItem, 'edit')}>
-                            <FoodItemResult foodItem={foodItem} />  
-                            <ListItemSecondaryAction onClick={() => props.handleDeleteFoodItem(foodItem)}>
-                                <IconButton edge="end" aria-label="delete">
-                                    <DeleteIcon />
-                                </IconButton>
-                            </ListItemSecondaryAction>  
-                        </ListItem>
-                    ))}
-                </List>
-            </Paper>
+            <List>
+                {props.foundFoodItems.map((foodItem) => 
+                (
+                    <ListItem button onClick={() => props.triggerFoodItemUpdate(foodItem, 'edit')}>
+                        <FoodItemResult foodItem={foodItem} />  
+                        <ListItemSecondaryAction onClick={() => props.handleDeleteFoodItem(foodItem)}>
+                            <IconButton edge="end" aria-label="delete">
+                                <DeleteIcon />
+                            </IconButton>
+                        </ListItemSecondaryAction>  
+                    </ListItem>
+                ))}
+            </List>
         }
     </div>
 );
