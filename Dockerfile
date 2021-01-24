e483c44856c4a0ee96a4179a52942b904ce59eda
Ian Wong <wong.iancw@gmail.com>
2021-01-24 17:56:34 +1100

    Making backend host a variable for deployment

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,9 @@ ADD . /app
 # Need to fix the following - we shouldn't need to build codebase here in docker...
 RUN yarn install && yarn run build
 
+# For deployment
+ENV DATA_HOST=127.0.0.1
+
 # Start the app from container
 EXPOSE 8080
 CMD [ "yarn", "run", "serve" ]
--- src/utils/DataClient.js
+++ src/utils/DataClient.js
@@ -6,7 +6,8 @@ import { GraphQLClient, gql } from 'graphql-request';
 // ------------------------
 
 // Backend URL for querying
-const BACKEND_URL = 'http://localhost:3000/graphql';
+const BACKEND_HOST = process.env.DATA_HOST || '127.0.0.1';
+const BACKEND_URL = `http://${BACKEND_HOST}:3000/graphql`;
 const GraphqlClient = new GraphQLClient(BACKEND_URL);
 
 // ------------------------
